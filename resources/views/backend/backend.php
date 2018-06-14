0414697c563db31f56e9207da767d6b318d1fa4b
linlm <linlm1994@gmail.com>
2018-06-14 11:20:07 +0800

    seo

--- resources/views/backend/backend.php
+++ resources/views/backend/backend.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?=csrf_token();?>
     <title>ububs个人博客--后台管理</title>
+    <meta name="keywords" content="个人博客,ububs个人博客,个人博客模板,ububs,php,php7,swoole,swoole框架,php技术,vuejs,javascript" />
+    <meta name="description" content="ububs个人博客，基于php和swoole扩展开发的技术站，记录了一个十八线小程序员的成长过程。" />
     <link rel="stylesheet" href="<?=webpackLoad('backend/app.css');?>">
 </head>
 <body>
